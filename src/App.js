f4438ff01cb98a16b7b970493245e7940b734864
abhi114 <nabhi531@gmail.com>
2022-01-21 19:10:26 +0530

    add speechly

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Grid } from '@material-ui/core';
 import Details from './components/Details/Details';
 import Main from './components/Main/Main';
 import useStyles from './styles';
+import {PushToTalkButton,PushToTalkButtonContainer,ErrorPanel} from '@speechly/react-ui';
 
 const App = () => {
     const classes  = useStyles();
@@ -26,6 +27,10 @@ const App = () => {
           <Details title="Expense" />
         </Grid>
       </Grid>
+      <PushToTalkButtonContainer>
+        <PushToTalkButton/>
+        <ErrorPanel/>
+      </PushToTalkButtonContainer>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
+import {SpeechProvider} from '@speechly/react-client';
 import { Provider } from './context/context';
 import App from './App';
 import'./index.css';
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId="a4555305-ea39-4a12-b806-8c551b4b9c09" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById("root")
 );
 
