a5ce37d80f6a1a4c1aa3d7e916f2ad62c2bd683a
abhi114 <nabhi531@gmail.com>
2022-01-22 15:53:53 +0530

    main componenet added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,6 +4,7 @@ import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
 import {ExpenseTrackerContext} from '../../context/context';
+import InfoCard from '../InfoCard';
 const Main = () => {
   const classes = useStyles();
   const {balance} = useContext(ExpenseTrackerContext);
@@ -15,7 +16,7 @@ const Main = () => {
           <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
           <Typography variant="subtitle1" style={{lineHeight:'1.5em',marginTop:'20px'}}>
             {/* info Card */}
-            Try Saying : Add income for $100 in Category Salary for Monday ...
+            <InfoCard/>
           </Typography>
           <Divider />
           <Form/>
